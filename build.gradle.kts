919b89e67c5ca0221d807634944cf7459c714c0b
FlavioPiermattei <flavio.piermattei@studenti.unicam.it>
2026-04-01 18:44:12 +0200

    Refactor project: replace old main with JavaFX application and FXML setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,15 +4,24 @@ plugins {
     id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
+group = "it.unicam"
+version = "1.0-SNAPSHOT"
+
 repositories {
     mavenCentral()
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(25))
+    }
+}
+
 javafx {
-    version = "21"
-    modules("javafx.controls")
+    version = "25"
+    modules("javafx.controls", "javafx.fxml")
 }
 
 application {
-    mainClass.set("it.unicam.gioco.Main")
+    mainClass.set("it.unicam.gioco.MainApplication")
 }
---
+++ src/main/java/it/unicam/gioco/MainApplication.java
@@ -0,0 +1,36 @@
+package it.unicam.gioco;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApplication extends Application{
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        System.out.println("start method called");
+        // Creates an FXMLLoader to load the main JavaFX layout from the FXML file located in the resources/fxml folder.
+        // Crea un FXMLLoader per caricare il layout principale di JavaFX dal file FXML situato nella cartella resources/fxml.
+        FXMLLoader loader = new FXMLLoader(
+                MainApplication.class.getResource("/fxml/main-view.fxml")
+        );
+
+
+        //Loads all the graphical content form the FXML file into a new Scene object
+        Scene scene = new Scene(loader.load(),800,600);
+        //Set the tile shown at the top of the application window
+        stage.setTitle("Unkown");
+        //Attaches the created scene to the main window (stage)
+        stage.setScene(scene);
+        //Prevents the user from resizing the application window
+        stage.setResizable(false);
+        //Make the window visible on the screen
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        //Launches the JavaFX runtime, which will then call the start() method.
+        launch(args);
+    }
+}
